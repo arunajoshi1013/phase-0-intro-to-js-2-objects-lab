764d40ad92d9291781f86d240b6bcba2cd39ef79
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-03-10 20:37:15 -0800

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+   name: "Sam",
+   streetAddress: "Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+   const copy = { ...employee };
+
+   copy[key] = value
+   return copy
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+   employee[key] = value
+
+   return employee
+}
+
+function deleteFromEmployeeByKey(employee, key)
+{
+   const copy = { ...employee };
+  
+   delete copy[key]
+
+   return copy 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+   delete employee[key]
+   return employee
+}
